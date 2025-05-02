3154378e82562980ebfdf4b4b2bc07051702f50f
Dmitry Kovalev <trigger1610@gmail.com>
2025-05-02 15:39:36 +0400

    Restyle not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,12 @@
+import './NotFound.scss';
+
 export default function NotFound() {
   return (
-    <div role="alert">
-      <h1>404</h1>
-      <p>Page not found. Try a cocktail from the sidebar.</p>
+    <div role='alert' className="notfound">
+      <h1 className="notfound__title">404</h1>
+      <p className="notfound__message">
+        Page not found. Try a cocktail from the sidebar.
+      </p>
     </div>
   );
 }
