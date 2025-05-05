da044913add0b46ece4ffef7f622e299e764765d
Dmitry Kovalev <trigger1610@gmail.com>
2025-05-05 19:04:49 +0400

    Prettify; Fix mock paths;

--- src/entities/drink/ui/DrinkCard/DrinkCard.tsx
+++ src/entities/drink/ui/DrinkCard/DrinkCard.tsx
@@ -1,4 +1,4 @@
-import { Drink } from "@/entities/drink/types";
+import { Drink } from '@/entities/drink/types';
 import styles from './DrinkCard.module.scss';
 
 export default function DrinkCard({ drink }: { drink: Drink }) {
